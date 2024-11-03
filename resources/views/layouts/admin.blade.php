19b5245e015a4c4d395e17ed7570949887c16dc1
99Noah99 <noahluis.dubail@gmail.com>
2024-11-03 21:51:43 +0100

    Ajout bibliothèque select2 pour menu deroulant recherche dans layout admin

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -21,6 +21,10 @@
     <link rel="stylesheet" href="{{ asset('css/main.css') }}">
     <link rel="icon" type="image/png" href="{{ asset('images/plane.png') }}" />
 
+    <!-- utilisation select2 pour menu déroulant avec barre de recherche -->
+    <link href="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/css/select2.min.css" rel="stylesheet" />
+    <script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js"></script>
+
     <title>Admin pages</title>
 </head>
 
